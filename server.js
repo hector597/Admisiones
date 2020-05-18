0e66963a0c343b70db24e9b6fd78f39034c2779d
hector597 <61427815+hector597@users.noreply.github.com>
2020-05-18 12:41:58 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser = require('bodyParser');
+const bodyParser = require('body-Parser');
 const multipart = require('connect-multiparty');
 const cors = require('cors');
 
