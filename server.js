f84a7fb14e16de5e61c7072b8ec2ee9a15750f7a
hector597 <61427815+hector597@users.noreply.github.com>
2020-05-18 11:07:12 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('bodyParser');
+const multipart = require('connect-multiparty');
+const cors = require('cors');
+
+const PORT = 3000;
+const app = express();
+
+app.use(cors());
+
+const multiPartMiddleware = multipart({
+    uploadDir: './almacenamiento'
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+//EndPoint to Upload files
+app.post('/api/subir', multiPartMiddleware, (req, res)=>{
+    res.json({
+        'message': 'Fichero subido correctamente!'
+    });
+}); 
+
+app.get('/', (req, res) => {
+    res.send('Hola mundo');
+});
+
+app.listen(PORT, () => console.log('App running on port: ${PORT}'));
+
+
